b073176fcc038ecdfe6136880073bef8a800783f
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-07-03 20:05:10 +0530

    connected. OTP not working

--- frontend/src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Input(props) {
-    return (
-        <input className=".inputBox" type={props.type} placeholder={props.placeholder} />
-    );
-}
-
-export default Input;
