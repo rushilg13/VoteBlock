0eeb481d3893576884834960dfa3b7ae3991eccf
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2021-07-05 17:30:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ getDocument();
 const login = async (emailId) => {
   const res = await Auth(emailId, "VOTEBLOCK");
   console.log(res.mail, res.OTP);
-  User_Details.updateOne({email: res.mail}, {otp: res.OTP}).then(function(data){
+  User_Detail.updateOne({email: res.mail}, {otp: res.OTP}).then(function(data){
   });
 }
 
